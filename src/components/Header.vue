ae2b8e2d2abb7f04710f9ce32d1b2233ee58f6a6
Thiago Giammattey <giammattey@gmail.com>
2019-03-01 13:28:52 -0300

    Accounts with Netlify

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home </router-link>|
+      <router-link to="/about">About </router-link>|
+      <a @click="onClick">Logout</a>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { auth } from "@/main";
+
+export default {
+  name: "Header",
+  methods: {
+    onClick() {
+      const user = auth.currentUser();
+
+      user
+        .logout()
+        .then(() => {
+          this.$router.push({
+            name: "signin",
+            params: { userLoggedOut: true }
+          });
+        })
+        .catch(error => {
+          alert("Error: ", error);
+        });
+    }
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+#nav {
+  a,
+  img {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
